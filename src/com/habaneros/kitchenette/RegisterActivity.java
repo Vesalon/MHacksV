3c15677cb914d4fc3be068012dad6c1adcb86d01
ritzjwa <ritzjwa@0587360802.wireless.umich.net>
2015-01-17 16:22:49 -0500

    push regex validification stuffies

--- src/com/habaneros/kitchenette/RegisterActivity.java
+++ src/com/habaneros/kitchenette/RegisterActivity.java
@@ -2,7 +2,9 @@ package com.habaneros.kitchenette;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class RegisterActivity extends Activity {
 
@@ -12,6 +14,17 @@ public class RegisterActivity extends Activity {
 		setContentView(R.layout.activity_register);
 	}
 	
+    public void toChooseRecipeTypeScreen(View view) {
+        Intent intent = new Intent(this, ChooseRecipeTypeActivity.class);
+        startActivity(intent);
+    }   
+	
+    private boolean testUsername(String username) {
+        return username.matches("\\w{6,12}");
+    }
+    private boolean testPassword(String password) {
+        return password.matches("[0-9]*|[a-z]*");    
+    }
 	
 
 }
