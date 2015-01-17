e66b260dfdc1cc2699b02c065a326e18fa18d0a4
Vesselin Grantcharov <vess13@gmail.com>
2015-01-17 14:25:09 -0500

    added Step and Recipe classes code

--- src/com/habaneros/kitchenette/Recipe.java
+++ src/com/habaneros/kitchenette/Recipe.java
@@ -1,5 +1,44 @@
 package com.habaneros.kitchenette;
 
+import java.util.ArrayList;
 public class Recipe {
 
+	private ArrayList<Step> steps;
+
+	public Recipe() {
+		 steps = new ArrayList<Step>();
+	}
+
+	public Recipe(ArrayList<Step> firstSteps) {
+		for (int i = 0; i < firstSteps.size(); i++) {
+			steps.add(firstSteps.get(i));
+		}
+	}
+
+	public Step getStep(int index) {
+		return steps.get(index);
+	}
+
+	public Step getFirstStep() {
+		return getStep(0);
+	}
+
+	public Step removeStep(int index) {
+		return steps.remove(index);
+	}
+
+	public Step removeFrontStep() {
+		return steps.remove(0);
+	}
+
+	public void addStep(String step) {
+		Step s = new Step(step);
+		steps.add(s);
+	}
+
+	public void addTimedStep(String step) {
+		Step s = new Step(step);
+		s.makeTiemd();
+		steps.add(s);
+	}
 }
--- src/com/habaneros/kitchenette/Step.java
+++ src/com/habaneros/kitchenette/Step.java
@@ -2,4 +2,15 @@ package com.habaneros.kitchenette;
 
 public class Step {
 
+	boolean timed;
+	String step;
+
+	public Step(String add) {
+		timed = false;
+		step = add;
+	}
+
+	public void makeTiemd() {
+		timed = true;
+	}
 }
